4be64c2829907434588bf49aabc246da0bab55d1
Brian Mulawka <mulawkabrian@gmail.com>
2025-07-11 11:09:48 +0200

    added 4 new screens

---
+++ src/surfaces/Favorites.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+export const Favorites = () => {
+    return (
+        <View>
+            <Text>this will be the Favorites screen</Text>
+        </View>
+    );
+};
